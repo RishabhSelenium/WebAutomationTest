88a9645eb82f7435283f23bad8df8088ae0baeb3
Rishabh <Rishabh.verma@reingames.com>
2022-06-28 02:41:56 +0530

    update file structure

--- src/test/java/org/testing/WebTest/webTest.java
+++ src/test/java/org/testing/WebTest/webTest.java
@@ -1,3 +1,4 @@
+package org.testing.WebTest;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -6,10 +7,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class webTest {
 
-public static void main(String[] args) throws InterruptedException {
+	
+	@Test
+public void WebTestCase() {
 	//invoke the chromeDriver to launch the chrome
 	System.setProperty("webdriver.chrome.driver", "../RishabhWebAssignment/drivers/chromedriver");
 	WebDriver driver = new ChromeDriver(); 
@@ -17,14 +21,14 @@ public static void main(String[] args) throws InterruptedException {
     driver.manage().window().maximize(); // maximize the chrome browser
     driver.manage().deleteAllCookies(); // 
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//    driver.switchTo().frame(driver.findElement(By.name("callout"))); 
-//    driver.findElement(By.xpath("//button[contains(@aria-label, 'No thanks')]")).click();
-//    driver.switchTo().parentFrame();
-    WebElement wb = driver.findElement(By.xpath("//*[@title='Search']")); //xpath of
+    
+    WebElement wb = driver.findElement(By.xpath("//*[@title='Search']")); //xpath of google search bar
     wb.sendKeys("Selenium");// typing Selenium in search bar
     wb.sendKeys(Keys.ENTER); //press enter 
-    driver.findElement(By.xpath("(//*[text()='Selenium'])[1]")).click();// xpath of sea
+    driver.findElement(By.xpath("(//*[text()='Selenium'])[1]")).click();// xpath to open https://www.selenium.dev
     
+    
+    // this code is to verify the testCase
     String expectedURL = "https://www.selenium.dev/";
     String expectedTitle = "Selenium";
     Assert.assertEquals(driver.getTitle(), expectedTitle);
